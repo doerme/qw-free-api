b712e3928a084fa33ff65fcbc986721303789603
doerme <398921432@qq.cpm>
2025-03-03 09:27:14 +0800

    first commit

---
+++ src/api/routes/chat.ts
@@ -0,0 +1,44 @@
+import _ from "lodash";
+
+import Request from "@/lib/request/Request.ts";
+import Response from "@/lib/response/Response.ts";
+import chat from "@/api/controllers/chat.ts";
+
+export default {
+  prefix: "/v1/chat",
+
+  post: {
+    "/completions": async (request: Request) => {
+      request
+        .validate('body.conversation_id', v => _.isUndefined(v) || _.isString(v))
+        .validate("body.messages", _.isArray)
+        // .validate("headers.authorization", _.isString);
+      // ticket切分
+      // request.headers.authorization = 'Bearer gsBH1opRO0qadzXjCc$oyjFCKpcoe63HQQr24bu0*CgFUn_V1zYOvIz9lixtxl1fbmzzp_8Gn1gy0'
+      // console.log(`request.headers.authorization`, request.headers.authorization)
+      const tokens = chat.tokenSplit(request.headers.authorization);
+      // 随机挑选一个ticket
+      const token = _.sample(tokens);
+      const { model, conversation_id: convId, messages, search_type, stream } = request.body;
+      if (stream) {
+        const stream = await chat.createCompletionStream(
+          model,
+          messages,
+          search_type,
+          token,
+          convId
+        );
+        return new Response(stream, {
+          type: "text/event-stream",
+        });
+      } else
+        return await chat.createCompletion(
+          model,
+          messages,
+          search_type,
+          token,
+          convId
+        );
+    },
+  },
+};
